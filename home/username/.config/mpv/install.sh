2f6c945a35016ac80f2a776c69ef2aa967817aa7
geekgit <geeekz@gmail.com>
2018-10-27 11:22:04 +0000

    mpv install script: redirect check

--- home/username/.config/mpv/install.sh
+++ home/username/.config/mpv/install.sh
@@ -5,12 +5,24 @@ if [[ -z "$MpvArg" ]]
 then
 	MpvTag=""
 	MpvDir=""
+	RedirectFlag=false
 else
 	MpvTag=".${MpvArg}"
 	MpvDir="${MpvArg}/"
+	RedirectFlag=true
+fi
+
+if [[ "$RedirectFlag" = false ]]
+then
+	unlink "mpv.conf"
+	unlink "input.conf"
 fi
 
 wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/home/username/.config/mpv/${MpvDir}mpv.conf${MpvTag}" -O "mpv.conf${MpvTag}"
 wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/home/username/.config/mpv/${MpvDir}input.conf${MpvTag}" -O "input.conf${MpvTag}"
-ln -sf "mpv.conf${MpvTag}" "mpv.conf"
-ln -sf "input.conf${MpvTag}" "input.conf"
+
+if [[ "$RedirectFlag" = true ]]
+then
+	ln -sf "mpv.conf${MpvTag}" "mpv.conf"
+	ln -sf "input.conf${MpvTag}" "input.conf"
+fi
